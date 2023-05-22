befa9b906f6caab06dc11af32aae58dd41e4bf9a
1antonioleite1 <128155482+1antonioleite1@users.noreply.github.com>
2023-05-21 22:12:57 -0300

    feito

--- main.py
+++ main.py
@@ -2,15 +2,12 @@
 
 import re
 
-from unidecode import unidecode
-
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
     n = string
     n = n.lower()
     n = n.replace(" ", "")
     n = re.sub(r'[^\w\s]','',n)
-    n = unidecode(n)
     n2 = n[::-1]
 
     if n == n2:
