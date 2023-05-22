80936ea64db52ae7f7f2f014c644932310412068
1antonioleite1 <128155482+1antonioleite1@users.noreply.github.com>
2023-05-21 21:43:40 -0300

    teste

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import re
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
     n = string
+    n = n.lower()
     n2 = n[::1]
     n2 = n2.replace(" ", "")
     n2 = re.sub(r'[^\w\s]','',n2)
+    n2 = n2.lower()
 
     if n == n2:
         return True
