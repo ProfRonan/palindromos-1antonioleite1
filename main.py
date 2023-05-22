9d38893c01c37763d9175a399fc2e5d3ce4fbc49
1antonioleite1 <128155482+1antonioleite1@users.noreply.github.com>
2023-05-21 21:33:45 -0300

    teste

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 """Main functions"""
 
+import re
 
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
-    return False
+    n = string
+    n2 = n[::1]
+    n2 = n2.replace(" ", "")
+    n2 = re.sub(r'[^\w\s]','',n2)
+
+    if n == n2:
+        return True
+    else:
+        return False
